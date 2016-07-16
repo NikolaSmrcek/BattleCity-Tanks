568c5ad216155532287af6a5aabe5c3139f65d7a
NikolaSmrcek <nsmrcek@gmail.com>
2016-07-16 14:34:04 +0200

    Tested importing texture from a tileset and making pixi movieClip. Changing animations on stage.

--- public/javascripts/app/game/game.component.js
+++ public/javascripts/app/game/game.component.js
@@ -19,22 +19,30 @@ System.register(['@angular/core'], function(exports_1, context_1) {
             }],
         execute: function() {
             GameComponent = (function () {
-                function GameComponent(element) {
-                    this.element = element;
+                function GameComponent(_element) {
+                    this._element = _element;
                     this.renderer = null;
                     this.stage = null;
                     this.texture = null;
                     this.bunny = null;
                     this.socket = null;
+                    this.tank = null;
+                    this.element = null;
                     this.renderer = null;
                     this.stage = null;
                     this.texture = null;
                     this.bunny = null;
+                    this.element = _element;
                     this.socket = io('http://localhost:8000');
                     //0x1099bb
+                    PIXI.loader.add('gameTileSet', '/public/assets/game/images/gameTileSet.png').load(this.onAssetsLoaded.bind(this));
+                }
+                GameComponent.prototype.onAssetsLoaded = function (loader, resources) {
+                    this.tank = {};
                     this.renderer = PIXI.autoDetectRenderer(800, 600, { backgroundColor: 0x000000 });
                     //adding game to custom selector 
                     //document.body.appendChild(this.renderer.view);
+                    //it was this.element had to change it cause this is promise asyinkronius funciton
                     this.element.nativeElement.appendChild((this.renderer.view));
                     // create the root of the scene graph
                     this.stage = new PIXI.Container();
@@ -48,29 +56,66 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                     // move the sprite to the center of the screen
                     this.bunny.position.x = 300;
                     this.bunny.position.y = 150;
+                    this.setBunny(this.bunny);
                     this.stage.addChild(this.bunny);
-                    //custom adding socket message behaviour
-                    /*
-                    this.socket.on('priceUpdate', function(data){
-                        this.bunny.position.x = data;
-                    }).bind(this);
-                       */
+                    var u = new SpriteUtilities(PIXI);
+                    //this.tank.walkLeft this.tank.walkUp
+                    //resources.gameTileSet.textures
+                    var textures = u.frames(resources.gameTileSet.texture, [[0, 0], [16, 0]], 16, 16);
+                    var textures2 = u.frames(resources.gameTileSet.texture, [[32, 0], [48, 0]], 16, 16);
+                    //this.tank.walkUp = u.sprite(textures);
+                    this.tank.walkUp = textures;
+                    this.tank.walkUp = new PIXI.extras.MovieClip(this.tank.walkUp);
+                    this.tank.walkUp.position.set(150);
+                    this.tank.walkUp.anchor.set(0.5);
+                    this.tank.walkUp.animationSpeed = 0.5;
+                    this.tank.walkLeft = textures2;
+                    this.tank.walkLeft = new PIXI.extras.MovieClip(this.tank.walkLeft);
+                    this.tank.walkLeft.position.set(150);
+                    this.tank.walkLeft.anchor.set(0.5);
+                    this.tank.walkLeft.animationSpeed = 0.5;
+                    //this.tank.walkUp.play();
+                    this.tank.currentAnimation = this.tank.walkUp;
+                    this.tank.currentAnimation.play();
+                    this.tank.walkLeft.visible = false;
+                    this.tank.walkUp.animationNameString = "walkUp";
+                    this.tank.walkLeft.animationNameString = "walkLeft";
+                    this.tank.walkUp.scale.set(2); //skaliranje slike
+                    this.tank.walkLeft.scale.set(2); //sklairanje slike (tenka)
+                    this.setTank(this.tank);
+                    this.stage.addChild(this.tank.currentAnimation);
+                    this.stage.addChild(this.tank.walkLeft);
+                    this.setupSockets();
+                    this.animate();
+                };
+                GameComponent.prototype.changePosition = function (newPos) {
+                    this.bunny.position.x = newPos;
+                    //primjer kako promjenit animaciju
+                    this.tank.currentAnimation.visible = false;
+                    this.tank.currentAnimation.stop();
+                    this.tank.walkLeft.visible = true;
+                    this.tank.currentAnimation = this.tank.walkLeft;
+                    this.tank.currentAnimation.play();
+                };
+                GameComponent.prototype.setupSockets = function () {
                     this.socket.on('priceUpdate', function (data) {
                         this.changePosition(data);
                     }.bind(this));
-                    // start animating
-                    this.animate();
-                }
-                GameComponent.prototype.changePosition = function (newPos) {
-                    this.bunny.position.x = newPos;
                 };
                 GameComponent.prototype.animate = function () {
                     //this.animate.bind(this) jer callback izgubi referencu
                     requestAnimationFrame(this.animate.bind(this));
                     // just for fun, let's rotate mr rabbit a little
                     this.bunny.rotation += 0.1;
+                    // this.tank.walkUp.rotation += 0.01;
                     // render the container
                     this.renderer.render(this.stage);
+                };
+                GameComponent.prototype.setTank = function (_tank) {
+                    this.tank = _tank;
+                };
+                GameComponent.prototype.setBunny = function (_bunny) {
+                    this.bunny = _bunny;
                 };
                 GameComponent = __decorate([
                     core_1.Component({
--- public/javascripts/app/game/game.component.ts
+++ public/javascripts/app/game/game.component.ts
@@ -2,8 +2,9 @@
  * Created by nikola on 4/29/16.
  */
 import {Component, ElementRef} from '@angular/core';
-declare var PIXI:any;
-declare var io:any;
+declare var PIXI: any;
+declare var io: any;
+declare var SpriteUtilities: any;
 
 @Component({
     selector: 'proba',
@@ -17,56 +18,119 @@ export class GameComponent {
     texture = null;
     bunny = null;
     socket = null;
+    tank = null;
+    element = null;
 
-    constructor(public element: ElementRef){
+    constructor(public _element: ElementRef) {
         this.renderer = null;
         this.stage = null;
         this.texture = null;
         this.bunny = null;
+        this.element = _element;
         this.socket = io('http://localhost:8000');
         //0x1099bb
-        this.renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x000000});
+
+        PIXI.loader.add('gameTileSet', '/public/assets/game/images/gameTileSet.png').load(this.onAssetsLoaded.bind(this));
+
+    }
+
+    onAssetsLoaded(loader, resources) {
+        this.tank = {};
+
+        this.renderer = PIXI.autoDetectRenderer(800, 600, { backgroundColor: 0x000000 });
 
         //adding game to custom selector 
 
         //document.body.appendChild(this.renderer.view);
+
+        //it was this.element had to change it cause this is promise asyinkronius funciton
         this.element.nativeElement.appendChild((this.renderer.view));
 
-// create the root of the scene graph
+        // create the root of the scene graph
         this.stage = new PIXI.Container();
 
-// create a texture from an image path
+        // create a texture from an image path
         this.texture = PIXI.Texture.fromImage('/public/images/bunny.png');
 
-// create a new Sprite using the texture
+        // create a new Sprite using the texture
         this.bunny = new PIXI.Sprite(this.texture);
 
-// center the sprite's anchor point
+        // center the sprite's anchor point
         this.bunny.anchor.x = 0.5;
         this.bunny.anchor.y = 0.5;
 
-// move the sprite to the center of the screen
+        // move the sprite to the center of the screen
         this.bunny.position.x = 300;
         this.bunny.position.y = 150;
 
+        this.setBunny(this.bunny);
         this.stage.addChild(this.bunny);
 
-        //custom adding socket message behaviour
-        /*
-        this.socket.on('priceUpdate', function(data){
-            this.bunny.position.x = data;
-        }).bind(this);
-           */
-        this.socket.on('priceUpdate', function(data){
-           this.changePosition(data);
-        }.bind(this));
 
-// start animating
+        var u = new SpriteUtilities(PIXI);
+
+        //this.tank.walkLeft this.tank.walkUp
+        //resources.gameTileSet.textures
+        var textures = u.frames(resources.gameTileSet.texture,
+            [[0, 0], [16, 0]],
+            16, 16
+        );
+
+        var textures2 = u.frames(resources.gameTileSet.texture,
+            [[32, 0], [48, 0]],
+            16, 16
+        );
+
+        //this.tank.walkUp = u.sprite(textures);
+        this.tank.walkUp = textures;
+        this.tank.walkUp = new PIXI.extras.MovieClip(this.tank.walkUp);
+
+        this.tank.walkUp.position.set(150);
+        this.tank.walkUp.anchor.set(0.5);
+        this.tank.walkUp.animationSpeed = 0.5;
+
+        this.tank.walkLeft = textures2;
+        this.tank.walkLeft = new PIXI.extras.MovieClip(this.tank.walkLeft);
+
+        this.tank.walkLeft.position.set(150);
+        this.tank.walkLeft.anchor.set(0.5);
+        this.tank.walkLeft.animationSpeed = 0.5;
+
+        //this.tank.walkUp.play();
+
+        this.tank.currentAnimation = this.tank.walkUp;
+        this.tank.currentAnimation.play();
+
+        this.tank.walkLeft.visible = false;
+
+        this.tank.walkUp.animationNameString = "walkUp";
+        this.tank.walkLeft.animationNameString = "walkLeft";
+        this.tank.walkUp.scale.set(2); //skaliranje slike
+        this.tank.walkLeft.scale.set(2); //sklairanje slike (tenka)
+
+        this.setTank(this.tank);
+        this.stage.addChild(this.tank.currentAnimation);
+        this.stage.addChild(this.tank.walkLeft);
+
+        this.setupSockets();
         this.animate();
     }
 
-    changePosition(newPos){
+    changePosition(newPos) {
         this.bunny.position.x = newPos;
+        //primjer kako promjenit animaciju
+        this.tank.currentAnimation.visible = false;
+        this.tank.currentAnimation.stop();
+        
+        this.tank.walkLeft.visible = true;
+        this.tank.currentAnimation = this.tank.walkLeft;
+        this.tank.currentAnimation.play();
+    }
+
+    setupSockets() {
+        this.socket.on('priceUpdate', function(data) {
+            this.changePosition(data);
+        }.bind(this));
     }
 
     animate() {
@@ -74,10 +138,18 @@ export class GameComponent {
         requestAnimationFrame(this.animate.bind(this));
 
         // just for fun, let's rotate mr rabbit a little
-            this.bunny.rotation += 0.1;
+        this.bunny.rotation += 0.1;
+        // this.tank.walkUp.rotation += 0.01;
 
         // render the container
-            this.renderer.render(this.stage);
+        this.renderer.render(this.stage);
+    }
+
+    setTank(_tank) {
+        this.tank = _tank;
+    }
+    setBunny(_bunny) {
+        this.bunny = _bunny;
     }
 
 }
